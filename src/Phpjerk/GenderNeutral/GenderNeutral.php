e01ea21a35b23ea071053a05bb437cf4de69cc9e
Phpjerk <phpjerk2@gmail.com>
2013-12-06 12:10:16 -0600

    Jesus christ why am I actually putting time into this thing?

--- src/Phpjerk/GenderNeutral/GenderNeutral.php
+++ src/Phpjerk/GenderNeutral/GenderNeutral.php
@@ -18,9 +18,9 @@ class GenderNeutral {
 	 * @param  string $text
 	 * @return string
 	 */
-	public function neutralize($text = NULL)
+	public function neuter($text = NULL)
 	{
-		if (is_null($text)) return 'Nothing to neutralize';
+		if (is_null($text)) return 'Nothing to neuter';
 		$pat = '/\b(?:' . join('|', $this->nono_words) . ')\b/i';
 		return preg_replace_callback(
 			$pat,
@@ -40,7 +40,13 @@ class GenderNeutral {
 	 */
 	public function setNonoWords($words = [])
 	{
-		$this->nono_words = $words;
+		if (!is_array($words)) throw new \InvalidArgumentException('NoNo Words needs to be an array. Stop sucking.');
+
+		// Escape or @SaraMG will punch my face
+		$this->nono_words = array_map(function($var) {
+				return preg_quote($var);
+			}, $words);
+
 		return $this;
 	}
 
@@ -52,7 +58,25 @@ class GenderNeutral {
 	 */
 	public function setGoodWord($word = '')
 	{
+		if (!is_string($word)) throw new \InvalidArgumentException('Good Word needs to be a string. RTFM.');
 		$this->good_word = $word;
 		return $this;
+	}
+
+	/**
+	 * Allow for neutralize method.
+	 *
+	 * @param  string  $method
+	 * @param  array  $parameters
+	 * @return void
+	 */
+	public function __call($method, $parameters)
+	{
+		if ($method == 'neutralize')
+		{
+			return $this->neuter($parameters[0]);
+		}
+
+		throw new \BadMethodCallException("'$method' method does not exist. WTF are you trying to do?!");
 	}
 }
--- tests/GenderTest.php
+++ tests/GenderTest.php
@@ -11,56 +11,91 @@ class GenderTest extends PHPUnit_Framework_TestCase
 
 	public function testEmptyNeutralizer()
 	{
-		$this->assertEquals('Nothing to neutralize', $this->gender->neutralize());
+		$this->assertEquals('Nothing to neuter', $this->gender->neuter());
 	}
 
 	public function testNeutralizeHe()
 	{
-		$this->assertEquals('Fuckface is a total skee-boney', $this->gender->neutralize('He is a total skee-boney'));
+		$this->assertEquals('Fuckface is a total skee-boney', $this->gender->neuter('He is a total skee-boney'));
 	}
 
 	public function testNeutralizeHim()
 	{
-		$this->assertEquals('Get Fuckface a slim jim sucka', $this->gender->neutralize('Get him a slim jim sucka'));
+		$this->assertEquals('Get Fuckface a slim jim sucka', $this->gender->neuter('Get him a slim jim sucka'));
 	}
 
 	public function testNeutralizeHer()
 	{
-		$this->assertEquals('I gave Fuckface a nice cup-a sizzle', $this->gender->neutralize('I gave her a nice cup-a sizzle'));
+		$this->assertEquals('I gave Fuckface a nice cup-a sizzle', $this->gender->neuter('I gave her a nice cup-a sizzle'));
 	}
 
 	public function testNeutralizeShe()
 	{
-		$this->assertEquals('Did Fuckface eat that bagel?', $this->gender->neutralize('Did she eat that bagel?'));
+		$this->assertEquals('Did Fuckface eat that bagel?', $this->gender->neuter('Did she eat that bagel?'));
 	}
 
 	public function testNeutralizeHeHimself()
 	{
-		$this->assertEquals('I would punch that turkey but Fuckface already did it Fuckface.', $this->gender->neutralize('I would punch that turkey but he already did it himself.'));
+		$this->assertEquals('I would punch that turkey but Fuckface already did it Fuckface.', $this->gender->neuter('I would punch that turkey but he already did it himself.'));
 	}
 
 	public function testNeutralizeSheHerself()
 	{
-		$this->assertEquals('I do not think Fuckface knows how to pop-a-lock Fuckface.', $this->gender->neutralize('I do not think she knows how to pop-a-lock herself.'));
+		$this->assertEquals('I do not think Fuckface knows how to pop-a-lock Fuckface.', $this->gender->neuter('I do not think she knows how to pop-a-lock herself.'));
 	}
 
 	public function testReplaceGoodWord()
 	{
 		$this->gender->setGoodWord('Winnie The Pooh');
-		$this->assertEquals('I did not see Winnie The Pooh slap that sombrero.', $this->gender->neutralize('I did not see him slap that sombrero.'));
+		$this->assertEquals('I did not see Winnie The Pooh slap that sombrero.', $this->gender->neuter('I did not see him slap that sombrero.'));
 		$this->gender->setGoodWord();
-		$this->assertEquals(' a beatboxer', $this->gender->neutralize('He a beatboxer'));
+		$this->assertEquals(' a beatboxer', $this->gender->neuter('He a beatboxer'));
 	}
 
 	public function testReplaceNonoWords()
 	{
 		$this->gender->setNonoWords(['pretzel', 'discoball']);
-		$this->assertEquals('I ripped off that Fuckface and straightup ate it like a Fuckface.', $this->gender->neutralize('I ripped off that discoball and straightup ate it like a pretzel.'));
+		$this->assertEquals('I ripped off that Fuckface and straightup ate it like a Fuckface.', $this->gender->neuter('I ripped off that discoball and straightup ate it like a pretzel.'));
 	}
 
 	public function testReplaceGoodWordAndNonoWords()
 	{
 		$this->gender->setGoodWord('President Taft')->setNonoWords(['mama', 'ring-a-ding']);
-		$this->assertEquals('Yo President Taft so loopy, she got a President Taft on the ding-dong.', $this->gender->neutralize('Yo mama so loopy, she got a ring-a-ding on the ding-dong.'));
+		$this->assertEquals('Yo President Taft so loopy, she got a President Taft on the ding-dong.', $this->gender->neuter('Yo mama so loopy, she got a ring-a-ding on the ding-dong.'));
+	}
+
+	public function testNeutralizeBackwardCompat()
+	{
+		$this->assertEquals('Hey Fuckface! Who got the low down on a puppy to snuggle?', $this->gender->neutralize('Hey ladies! Who got the low down on a puppy to snuggle?'));
+	}
+
+	public function testBadMethodException()
+	{
+		$this->setExpectedException('BadMethodCallException');
+		$this->gender->numnums();
+	}
+
+	public function testNonoWordsInvalidArgumentException()
+	{
+		$this->setExpectedException('InvalidArgumentException');
+		$this->gender->setNonoWords('hip hop, you don\'t stop');
+	}
+
+	public function testGoodWordInvalidArgumentExceptionForInt()
+	{
+		$this->setExpectedException('InvalidArgumentException');
+		$this->gender->setGoodWord(37);
+	}
+
+	public function testGoodWordInvalidArgumentExceptionForArray()
+	{
+		$this->setExpectedException('InvalidArgumentException');
+		$this->gender->setGoodWord(['granny', 'ate', 'butterscotch']);
+	}
+
+	public function testEscapeRegexInNonoWords()
+	{
+		$this->gender->setNonoWords([".*"]);
+		$this->assertEquals('I let him poop on a daisy.', $this->gender->neuter('I let him poop on a daisy.'));
 	}
 }
